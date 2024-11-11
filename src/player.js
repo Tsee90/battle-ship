4e61f8944114ce6f5f4b305ecec58ffb9f95df46
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-11-11 15:43:41 -0500

    feat: random placement for computer

--- src/player.js
+++ src/player.js
@@ -26,11 +26,38 @@ export class Computer extends Player {
   }
 
   placeAll() {
-    this.gameboard.place(this.gameboard.getCarrier(), [0, 0], 'south');
-    this.gameboard.place(this.gameboard.getBattleship(), [1, 0], 'south');
-    this.gameboard.place(this.gameboard.getDestroyer(), [2, 0], 'south');
-    this.gameboard.place(this.gameboard.getSubmarine(), [3, 0], 'south');
-    this.gameboard.place(this.gameboard.getPatrol(), [4, 0], 'south');
+    this.placeRandom(this.gameboard.getCarrier());
+    this.placeRandom(this.gameboard.getBattleship());
+    this.placeRandom(this.gameboard.getDestroyer());
+    this.placeRandom(this.gameboard.getSubmarine());
+    this.placeRandom(this.gameboard.getPatrol());
+  }
+
+  placeRandom(ship) {
+    let placement = this.gameboard.place(
+      ship,
+      this.randomCoordinate(),
+      this.randomOrientation()
+    );
+    while (placement === null) {
+      placement = this.gameboard.place(
+        ship,
+        this.randomCoordinate(),
+        this.randomOrientation()
+      );
+    }
+  }
+
+  randomCoordinate() {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    return [x, y];
+  }
+
+  randomOrientation() {
+    const list = ['north', 'south', 'east', 'west'];
+    const randomNum = Math.floor(Math.random() * 5);
+    return list[randomNum];
   }
 
   attack() {
